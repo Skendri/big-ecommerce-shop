c848003e9cc28c8b0116f9680079e4fd8f365eee
Skendri <skendripeza0@gmail.com>
2025-06-07 16:40:29 +0200

    commit new

--- javascript-pages/servisi.js
+++ javascript-pages/servisi.js
@@ -103,3 +103,25 @@ mbeshtjellesiKartaveServisi.addEventListener("mouseenter", () =>
   clearTimeout(timeoutIDServis)
 );
 mbeshtjellesiKartaveServisi.addEventListener("mouseleave", autoPlayServisi);
+
+
+//       kontenti dynamic
+const countersEl = document.querySelectorAll(".counter");
+
+countersEl.forEach((counterEl) => {
+  counterEl.innerText = "0";
+  incrementCounter();
+  function incrementCounter() {
+    let currentNum = +counterEl.innerText;
+    const dataCeil = counterEl.getAttribute("data-ceil");
+    const increment = dataCeil / 15;
+    currentNum = Math.ceil(currentNum + increment);
+
+    if (currentNum < dataCeil) {
+      counterEl.innerText = currentNum;
+      setTimeout(incrementCounter, 50);
+    } else {
+      counterEl.innerText = dataCeil;
+    }
+  }
+});
--- javascript-pages/suportiKlientitn.js
+++ javascript-pages/suportiKlientitn.js
@@ -84,3 +84,32 @@
         });      
 
         //text carousel te suporti-klientit.php
+
+        // feedbacku qe do lene userat per eksperiencen e blerjeve ne faqe
+
+        const starsEl = document.querySelectorAll(".fa-star");
+        const emojisEl = document.querySelectorAll(".far");
+        const colorsArray = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+        updateRating(0);
+
+        starsEl.forEach((starEl, index) => {
+        starEl.addEventListener("click", () => {
+            updateRating(index);
+        });
+        });
+
+        function updateRating(index) {
+        starsEl.forEach((starEl, idx) => {
+            if (idx < index + 1) {
+            starEl.classList.add("active");
+            } else {
+            starEl.classList.remove("active");
+            }
+        });
+
+        emojisEl.forEach((emojiEl) => {
+            emojiEl.style.transform = `translateX(-${index * 50}px)`;
+            emojiEl.style.color = colorsArray[index];
+        });
+        }
