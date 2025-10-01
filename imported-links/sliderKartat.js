4737e94ce094d3c2a3ddf636a7c4b41fa7f0cb03
skendri <skendripeza0@gmail.com>
2025-10-02 00:52:26 +0200

    stuck opp

--- imported-links/sliderKartat.js
+++ imported-links/sliderKartat.js
@@ -97,3 +97,67 @@ export const kartatRreshkitese = [
         paragrafi4: "Zgjidhni nga 12 deri ne 48 keste"
     }
 ];
+
+
+const kontainerKartat = document.getElementById("kartat-e-medha");
+kontainerKartat.innerHTML = " ";
+
+kartatRreshkitese.forEach((kartat, id) => {
+    const diviKartave = document.createElement("div");
+    diviKartave.className = "kartaMadhe";
+
+    diviKartave.innerHTML += `
+    <div class="kartaMadhe-foto">
+        <img src="${kartat.img}" alt="" draggable="false" />
+    </div>
+    
+    <div class="kartaMadhe-kutiaInfos">
+        <div class="kartaMadhe-info">
+            <p>${kartat.paragrafi1}</p>
+            <p>${kartat.paragrafi2}</p>
+        </div>
+        
+        <div class="kartaMadhe-infoDyte">
+            <h4>${kartat.header1}</h4>
+            <h3>${kartat.header2}</h3>
+            <p>${kartat.paragrafi3}</p>
+            
+            <div class="kartaMadhe-info">
+                <p>${kartat.paragrafi4}</p>
+            </div>
+        </div>
+    </div> `;
+    
+    kontainerKartat.appendChild(diviKartave)
+})
+
+// HTML e kompnentit te sliderit te pare te kartave te index.html
+
+    // <div class="kartaMadhe" id="kartaMadhe">
+    //     <div class="kartaMadhe-foto">
+    //         <img src="https://cms-images.mmst.eu/hzxov1nxpus5/278L0ptpEu27yrj4qm4yc6/b236a4aba8cfb8c25410d4a7ff9f26b1/visual-principale-lp-foldable-desk-centrato.jpg?q=70&fm=webp"
+    //         alt=""
+    //         draggable="false" />
+    //     </div>
+        
+    //     <div class="kartaMadhe-kutiaInfos">
+    //         <div class="kartaMadhe-info">
+    //             <p>Ne dyqan dhe online</p>
+    //             <p>Deri me 9 gusht</p>
+    //         </div>
+            
+    //         <div class="kartaMadhe-infoDyte">
+    //             <h4>PSE TE ZGJIDHNI SAMSUNG AI PER SHTEPIN TUAJ?</h4>
+    //             <h3>PERFITO 10'000 LEKE ULJE!!</h3>
+    //             <p>
+    //                 Per cdo blerje Samsung Galaxy Z FOLD6/ Z FLIP6 Lorem ipsum dolor
+    //                 sit, amet consectetur adipisicing elit. Debitis quos eum
+    //                 voluptatum molestias delectus
+    //             </p>
+                
+    //             <div class="kartaMadhe-info">
+    //                 <p>Zgjidhni nga 12 deri ne 48 keste</p>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div> 
--- imported-links/sliderTelefonat.js
+++ imported-links/sliderTelefonat.js
@@ -87,4 +87,61 @@
 //         linku: "Informacion mbi: Financimi, Cmimi, Sasia, Koha e dorezimit dheMarrja",
 //         paragrafi4: "Eko-kontributi i TVSH dhe WEEE i perfshire",
 //     }
-// ]
+// ];
+
+//     const kontenieriTelefonat = document.getElementById("kartat-plan-pare");
+//     kontenieriTelefonat.innerHTML = " ";
+
+//     sliderTelefonat.forEach((telefonat, id) => {
+//         const diviTelefonat = document.createElement("div");
+//         diviTelefonat.className = "karta-plan-pare";
+//         // diviTelefonat.id = "kartat-${id}"
+
+//         diviTelefonat.innerHTML += `
+//             <div class="plani-pare-siper" id="${telefonat.id}">
+//                 <h4>${telefonat.header1}</h4>
+//                 <img src="${telefonat.img}"
+//                 alt=""
+//                 />
+                
+//                 <div class="reviews">
+//                     <img src="${telefonat.imgRating}" />
+//                     <p>${telefonat.paragrafi1}</p>
+//                 </div>
+//                 <p>${telefonat.paragrafi2}</p>
+//             </div>
+            
+//             <div class="plani-pare-posht">
+//                 <p>${telefonat.paragrafi3.paragrafi} <br /> <span class="cmimi">${telefonat.paragrafi3.span}</span> </p>
+//                 <a href="" >${telefonat.linku}</a>
+//                 <p>E${telefonat.paragrafi4}</p>
+//             </div>`;
+
+//             kontenieriTelefonat.appendChild(diviTelefonat);
+//     })
+
+
+
+
+
+
+    // <div class="karta-plan-pare">
+    //     <div class="plani-pare-siper">
+    //         <h4>Top shites</h4>
+    //         <img src="https://images.samsung.com/is/image/samsung/p6pim/al/sm-f741bzsgeuc/gallery/al-galaxy-zflip6-f741-sm-f741bzsgeuc-542935364?$684_547_JPG$"
+    //         alt=""
+    //         />
+            
+    //         <div class="reviews">
+    //             <img src="./images/rating-35.png" alt="starts" />
+    //             <p>200</p>
+    //         </div>
+    //         <p>APPLE AirPods Gjenerata e 2-të me kuti karikimi (modeli 2019)</p>
+    //     </div>
+        
+    //     <div class="plani-pare-posht">
+    //         <p>Rekomanduar €979 <br /> <span class="cmimi">799,-€</span> </p>
+    //         <a href="" >Informacion mbi: Financimi, Cmimi, Sasia, Koha e dorezimit dhe Marrja</a>
+    //         <p>Eko-kontributi i TVSH dhe WEEE i perfshire</p>
+    //     </div>
+    // </div>
